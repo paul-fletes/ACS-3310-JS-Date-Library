66f230db81ded2c70dcdbd18f6d3a222d7002910
Paul Fletes <paul.fletes@gmail.com>
2023-09-06 12:07:00 -0500

    complete challenge 4

--- src/index.js
+++ src/index.js
@@ -98,6 +98,61 @@ class D {
     const suffix = (relevantDigits <= 3) ? suffixes[relevantDigits - 1] : suffixes[3];
     return `${date}${suffix}`;
   };
+
+  // when() method that takes a date and returns a human readable string of 'when' that date will occur.
+  when() {
+    const currentDate = new Date();
+    const timeDifference = this._date - currentDate;
+    const absTimeDifference = Math.abs(timeDifference);
+
+    const oneMinute = 60 * 1000;
+    const oneHour = oneMinute * 60;
+    const oneDay = oneHour * 24;
+    const oneMonth = oneDay * 30; // Approximate
+    const oneYear = oneDay * 365; // Approximate
+
+    if (timeDifference < 0) {
+      if (absTimeDifference < oneMinute) {
+        return "just now";
+      } else if (absTimeDifference < oneHour) {
+        const minutesAgo = Math.floor(absTimeDifference / oneMinute);
+        return `${minutesAgo} ${minutesAgo === 1 ? 'minute' : 'minutes'} ago`;
+      } else if (absTimeDifference < oneDay) {
+        const hoursAgo = Math.floor(absTimeDifference / oneHour);
+        return `${hoursAgo} ${hoursAgo === 1 ? 'hour' : 'hours'} ago`;
+      } else if (absTimeDifference < oneMonth) {
+        const daysAgo = Math.floor(absTimeDifference / oneDay);
+        return `${daysAgo} ${daysAgo === 1 ? 'day' : 'days'} ago`;
+      } else if (absTimeDifference < oneYear) {
+        const monthsAgo = Math.floor(absTimeDifference / oneMonth);
+        return `${monthsAgo} ${monthsAgo === 1 ? 'month' : 'months'} ago`;
+      } else {
+        const yearsAgo = Math.floor(absTimeDifference / oneYear);
+        return `${yearsAgo} ${yearsAgo === 1 ? 'year' : 'years'} ago`;
+      }
+    } else if (timeDifference > 0) {
+      if (absTimeDifference < oneMinute) {
+        return "just now";
+      } else if (absTimeDifference < oneHour) {
+        const minutesFromNow = Math.floor(absTimeDifference / oneMinute);
+        return `in ${minutesFromNow} ${minutesFromNow === 1 ? 'minute' : 'minutes'}`;
+      } else if (absTimeDifference < oneDay) {
+        const hoursFromNow = Math.floor(absTimeDifference / oneHour);
+        return `in ${hoursFromNow} ${hoursFromNow === 1 ? 'hour' : 'hours'}`;
+      } else if (absTimeDifference < oneMonth) {
+        const daysFromNow = Math.floor(absTimeDifference / oneDay);
+        return `in ${daysFromNow} ${daysFromNow === 1 ? 'day' : 'days'}`;
+      } else if (absTimeDifference < oneYear) {
+        const monthsFromNow = Math.floor(absTimeDifference / oneMonth);
+        return `in ${monthsFromNow} ${monthsFromNow === 1 ? 'month' : 'months'}`;
+      } else {
+        const yearsFromNow = Math.floor(absTimeDifference / oneYear);
+        return `in ${yearsFromNow} ${yearsFromNow === 1 ? 'year' : 'years'}`;
+      }
+    } else {
+      return "just now";
+    }
+  };
 };
 
 const no_param_date = new D();
@@ -130,13 +185,28 @@ console.log('Get the second: ', challengeTwoDate.secs);
 console.log('--------- Challenge 3 --------')
 
 const d = new D(2017, 0, 2, 3, 4, 5);
-console.log(d.format());          // Output: 2017 January 02
-console.log(d.format('y/m/d'));   // Output: 17/Jan/2
-console.log(d.format('H:I:S'));   // Output: 03:04:05
-console.log(d.format('h:i:s'));   // Output: 3:4:5
-console.log(d.format('Y-M-D h:I:S')); // Output: 2017-January-02 3:04:05
+console.log(d.format());
+console.log(d.format('y/m/d'));
+console.log(d.format('H:I:S'));
+console.log(d.format('h:i:s'));
+console.log(d.format('Y-M-D h:I:S'));
 
+// Create a when() method that takes a date and returns a human readable string of 'when' that date will occur.
+console.log('--------- Challenge 4 --------')
+const d1 = new D(2019, 0, 2, 3, 4, 5);
+console.log(d1.when());
 
+const d2 = new D(2019, 9, 2, 3, 4, 5);
+console.log(d2.when());
+
+const d3 = new D(2024, 9, 2, 3, 4, 5);
+console.log(d3.when());
+
+const d4 = new D(2019, 6, 30, 3, 4, 5);
+console.log(d4.when());
+
+const d5 = new D();
+console.log(d5.when());
 
 module.exports = {
   months,
